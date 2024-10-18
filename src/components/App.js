2205064573f56d2f23273cb9c5d1d4672c53a536
Qajay0832 <qajay0832@gmail.com>
2024-10-18 06:04:53 +0530

    sdecond

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   const [para,setPara]=useState(false);
   return (
     <div id="main">
-      <p id="para">{para?"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy":''}</p>
+      {para?<p id="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>:''}
       <button id="click" onClick={()=>setPara(true)}>Click Me</button>
     </div>
   );
